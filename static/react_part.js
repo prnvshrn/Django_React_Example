f2197f06ea0209e3513dc1fe76bf0b719a0ef738
prnvshrn <prnvshrn@gmail.com>
2017-12-23 23:57:37 +0530

    React Samples

---
+++ static/react_part.js
@@ -0,0 +1,8 @@
+class Test extends React.Component{
+    render()
+    {
+        return(
+            <h2>Testing React</h2>
+        );
+    }
+}
---
+++ static/react_part2.js
@@ -0,0 +1,10 @@
+var Movie = React.createClass({
+    render:function(){
+            return(
+                <div>
+                    <h2>Hello there</h2>
+                    <h2>This is a sample</h2>
+                </div>
+            );
+        }
+});
