51d6ca7dd91bc4c363266c7257af6b925db71932
prnvshrn <prnvshrn@gmail.com>
2017-12-23 11:26:51 +0530

    Initial setup

---
+++ D_R/views.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def openWelcome(request):
+    template = loader.get_template('test.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
